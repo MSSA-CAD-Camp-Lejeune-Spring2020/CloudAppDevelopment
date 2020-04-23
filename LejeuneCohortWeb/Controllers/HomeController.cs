0962f06a671c1e8862d2035979d6be13f624d5b2
Kyle Noland <31088025+kylednoland@users.noreply.github.com>
2020-04-23 15:03:30 -0500

    Added about (#43)
    * Added about
    
    * A better description
    
    Co-authored-by: Kyle Noland <kynoland@microsoft.com>

--- LejeuneCohortWeb/Controllers/HomeController.cs
+++ LejeuneCohortWeb/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace LejeuneCohortWeb.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
